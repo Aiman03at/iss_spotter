ca031aa7a5744bdb010476831fc9107881226280
Aiman03at <aiman03at@gmail.com>
2024-06-18 10:23:23 -0400

    Implement a function that returns a list of upcoming ISS passes for a particular location formatted as JSON.

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
 
 const {fetchMyIP}= require("./iss");
 const {fetchCoordsByIP} = require("./iss");
+const {fetchISSFlyOverTimes} =require("./iss");
+
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned IP:' , ip); 
 });
 
 fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -20,3 +23,13 @@ fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
 
   console.log('It worked! Returned coordinates:' , coordinates);
 });
+
+fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' },(error,res) =>{
+
+if(error) {
+  console.log("It didn't work", errorr);
+  return;
+
+}
+console.log("It worked !", res);
+}) 
--- iss.js
+++ iss.js
@@ -49,12 +49,13 @@ needle.get(`http://ipwho.is/${ip}`,(error, response, body) => {
     return;
   }
  //// checking for invalid ip address
+ 
   if (body.success === false) {
     return callback(new Error('Invalid IP address provided.'));
 }
   ///if the successfull
   
-    console.log("Successfull");
+    
     const latitude = body.latitude
     const longitude = body.longitude
     callback(null, {latitude, longitude});
@@ -62,4 +63,47 @@ needle.get(`http://ipwho.is/${ip}`,(error, response, body) => {
  });
  }
 
-module.exports = {fetchMyIP , fetchCoordsByIP };
+
+
+
+
+
+
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+  needle.get(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`,(error, response, body) => {
+  
+    if (error) 
+      {callback(error,null);
+      }
+    ///if response is not successfulll
+
+    if(response.statusCode !== 200) {
+      callback(Error(`Status code ${response.statusCode}when fetching coordinates`), null);
+      return;
+    }
+   //// checking for invalid ip address
+   
+    if (body.success === false) {
+      return callback(new Error('Invalid IP address provided.') ,null);
+  }
+  
+ const res =body.response;
+ callback(null,res);
+
+
+
+});
+};
+
+module.exports = {fetchMyIP , fetchCoordsByIP ,fetchISSFlyOverTimes};
